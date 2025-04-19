6fae162fabbb6e84392f5f56fd49ec700ce1d0aa
Phạm Duy Giang <phamgiang131205@gmail.com>
2025-04-19 10:24:56 +0700

    Add files via upload

--- day con chung.cpp
+++ day con chung.cpp
@@ -1,24 +1,39 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
+
+#define ll long long
+const int MOD = 1e9+7;
+
+void test()
+{
 	int t; cin>>t;
-	while(t--){
-		int n,m,k;
-		cin>>n>>m>>k;
+	while(t--)
+	{
+		int n,m,k; cin>>n>>m>>k;
 		int a[n],b[m],c[k];
-		for(int &x :a) cin>>x;
+		for(int &x : a) cin>>x;
 		for(int &x :b) cin>>x;
-		for(int &x :c) cin>>x;
-		int i = j = h =0;
-		vector<int> v;
-		while(i < n && j < m && h< k){
-			if(a[i] == b[j] && b[j] == c[h]) {
-				v.push_back(a[i]); i++; j++ ; h++;
-				
+		for(int &x:c) cin>>x;
+		int i=0,j=0,h=0; bool check = false;
+		while(i<n && j<m && h<k	)
+		{
+			if(a[i] == b[j] && b[j] == c[h] ) 
+			{
+				cout<<a[i]<<' ';i++;h++;j++; check = true;
 			}
-			else if(a[i] > b[j] && a[i]  > c[h]) i++ ;
-			else if(b[j] > a[i] && b[j] > c[h]) j++;
-			else if(c[h] > a[i] && c[h] > b[j] ) h++;
+			else if(a[i] < b[j] || a[i] < c[h]) i++;
+			else if(b[j] < a[i] || b[j] < c[h] ) j++;
+			else if(c[h] < a[i] || c[h] < b[j] ) h++;
 		}
+		if(!check) cout<<"NO";
+ 		cout<<endl;
 	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	test();
 }
