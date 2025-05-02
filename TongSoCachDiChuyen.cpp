a435c8f1f13c62bde0032ef9d94971926cb81936
Phạm Duy Giang <phamgiang131205@gmail.com>
2025-05-02 12:49:30 +0700

    Add files via upload

---
+++ TongSoCachDiChuyen.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int MOD = 1e9+7;
+int dp[100001];
+void test()
+{
+    int t; cin>>t;
+    while(t--) {
+        memset(dp,0,sizeof(dp));
+        int n,k; cin>>n>>k;
+        dp[0]=1;dp[1]=1;
+        for(int i=2;i<=n;i++) {
+            for(int j=1;j<=k;j++) {
+                if(i-j >=0) dp[i]=((dp[i]%MOD)+(dp[i-j]%MOD)) %MOD; 
+            }
+        }
+        cout<<dp[n]<<'\n';
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    
+    test();
+}
