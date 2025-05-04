b83b676d654732c87145e0985c7f5483f7d16b77
Phạm Duy Giang <phamgiang131205@gmail.com>
2025-05-04 22:50:55 +0700

    Add files via upload

---
+++ day con co tong le.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int MOD = 1e9+7;
+void f(int a[],int i,int n,vector<int> &v,vector<string> &result){
+	if(i==n) {
+		int sum =0;
+		for(int x:v) sum+=x;
+		if(sum%2==1) {
+			string s="";
+			for(int x:v) {
+				s+=to_string(x) +" ";
+			} result.push_back(s);
+		} return ;
+	}
+	v.push_back(a[i]);
+	f(a,i+1,n,v,result);
+	v.pop_back();
+	f(a,i+1,n,v,result);
+}
+
+void test()
+{
+	int t; cin>>t;
+	while(t--) {
+		vector<string> result;
+		int n; cin>>n;
+		int a[n];
+		for(int &i:a) cin>>i;
+		sort(a,a+n,greater<int>());
+		vector<int> v;
+		f(a,0,n,v,result);
+		for(int i=result.size()-1;i>=0;i--) {
+			cout<<result[i]<<'\n';
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	
+	test();
+}	
