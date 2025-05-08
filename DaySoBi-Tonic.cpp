3a9a1c2355dbba37b5fb091599b995649169bcd5
Phạm Duy Giang <phamgiang131205@gmail.com>
2025-05-09 00:24:09 +0700

    Update DaySoBi-Tonic.cpp

--- DaySoBi-Tonic.cpp
+++ DaySoBi-Tonic.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 #define ll long long
 const int MOD = 1e9+7;
-int f(vector<int> &a,vector<int> &b,int n) {
-    vector<int> dp1 =a,dp2=b; int res=0;
+int f(vector<int> &a,int n) {
+    vector<int> dp1 =a,dp2=a; int res=0;
     for(int i=1;i<n;i++) {
         for(int j=0;j<i;j++) {
             if(a[j] <a[i] && dp1[i] < dp1[j] +a[i] ) dp1[i] = dp1[j] +a[i];
@@ -12,7 +12,7 @@ int f(vector<int> &a,vector<int> &b,int n) {
     }
     for(int i=n-1;i>=0;i--) {
         for(int j=n-1;j>i;j--) {
-            if(b[j] < b[i] && dp2[i] < dp2[j] +b[i] ) dp2[i] = dp2[j] +b[i];
+            if(a[j] <a[i] && dp2[i] < dp2[j] +a[i] ) dp2[i] = dp2[j] +a[i];
         }
     }
     for(int i=0;i<n;i++) {
@@ -26,9 +26,9 @@ void test()
     int t; cin>>t;
     while(t--) {
         int n; cin>>n;
-        vector<int> a(n),b(n);
-        for(int &i:a) cin>>i; b=a;
-        cout<<f(a,a,n)<<'\n';
+        vector<int> a(n);
+        for(int &i:a) cin>>i; 
+        cout<<f(a,n)<<'\n';
     }
 }
 
